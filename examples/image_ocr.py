c412d0a036e4ecb5782ac855d349433b3a95e74a
caslei <lei.wang.sibet@gmail.com>
2018-10-27 22:48:02 -0400

    base class 'keras.callbacks.Callback'

--- examples/image_ocr.py
+++ examples/image_ocr.py
@@ -372,17 +372,19 @@ def ctc_lambda_func(args):
     # the 2 is critical here since the first couple outputs of the RNN
     # tend to be garbage:
     y_pred = y_pred[:, 2:, :]
+    # where is 'K.ctc_batch_cost' from????
     return K.ctc_batch_cost(labels, y_pred, input_length, label_length)
 
 
 # For a real OCR application, this should be beam search with a dictionary
-# and language model.  For this example, best path is sufficient.
+# and language model. For this example, best path is sufficient.
 
 def decode_batch(test_func, word_batch):
-    out = test_func([word_batch])[0]
+    out = test_func([word_batch])[0] # [word_batch]
     ret = []
     for j in range(out.shape[0]):
         out_best = list(np.argmax(out[j, 2:], 1))
+        # 'itertools.groupby' function
         out_best = [k for k, g in itertools.groupby(out_best)]
         outstr = labels_to_text(out_best)
         ret.append(outstr)
@@ -393,8 +395,7 @@ class VizCallback(keras.callbacks.Callback):
 
     def __init__(self, run_name, test_func, text_img_gen, num_display_words=6):
         self.test_func = test_func
-        self.output_dir = os.path.join(
-            OUTPUT_DIR, run_name)
+        self.output_dir = os.path.join(OUTPUT_DIR, run_name)
         self.text_img_gen = text_img_gen
         self.num_display_words = num_display_words
         if not os.path.exists(self.output_dir):
@@ -417,13 +418,16 @@ class VizCallback(keras.callbacks.Callback):
             num_left -= num_proc
         mean_norm_ed = mean_norm_ed / num
         mean_ed = mean_ed / num
+
         print('\nOut of %d samples:  Mean edit distance:'
               '%.3f Mean normalized edit distance: %0.3f'
               % (num, mean_ed, mean_norm_ed))
 
     def on_epoch_end(self, epoch, logs={}):
+        # 'save_weights()' and 'weights%02d.h5' % (epoch)
         self.model.save_weights(
             os.path.join(self.output_dir, 'weights%02d.h5' % (epoch)))
+
         self.show_edit_distance(256)
         word_batch = next(self.text_img_gen)[0]
         res = decode_batch(self.test_func,
@@ -434,15 +438,17 @@ class VizCallback(keras.callbacks.Callback):
             cols = 1
         for i in range(self.num_display_words):
             pylab.subplot(self.num_display_words // cols, cols, i + 1)
+
             if K.image_data_format() == 'channels_first':
                 the_input = word_batch['the_input'][i, 0, :, :]
             else:
                 the_input = word_batch['the_input'][i, :, :, 0]
+
             pylab.imshow(the_input.T, cmap='Greys_r')
-            pylab.xlabel(
-                'Truth = \'%s\'\nDecoded = \'%s\'' %
-                (word_batch['source_str'][i], res[i]))
-        fig = pylab.gcf()
+            pylab.xlabel( 'Truth = \'%s\'\nDecoded = \'%s\'' %
+                          (word_batch['source_str'][i], res[i]))
+
+        fig = pylab.gcf() # gcf ==> get current figure
         fig.set_size_inches(10, 13)
         pylab.savefig(os.path.join(self.output_dir, 'e%02d.png' % (epoch)))
         pylab.close()
